6cd6ced667b1bcf068aa3b9e8baf7fb688a1ffea
Mario Perez Hurtado <marioph10@gmail.com>
2024-01-15 11:14:30 +0100

    fix: protect routes with sveltekit hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,16 +4,56 @@ import { router } from "$lib/trpc/root";
 import { auth } from "$lib/auth/auth";
 import { redirect, type Handle } from "@sveltejs/kit";
 
+const APP_ROUTES = [
+  "/profile",
+  "/contacts",
+  "/settings",
+]
+
+const AUTH_ROUTES = [
+  "/login",
+  "/create-account",
+  "/email-verification",
+  "/forgot-password",
+]
+
+function pathRequiresAuth(path: string) {
+  if (path === "/") {
+    return true;
+  }
+  return APP_ROUTES.some(route => path.startsWith(route));
+}
+
+function pathRequiresNoAuth(path: string) {
+  return AUTH_ROUTES.some(route => path.startsWith(route));
+}
+
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.auth = auth.handleRequest(event);
   event.locals.session = await event.locals.auth.validate();
 
-  if (event.url.pathname.startsWith("/chat")) {
-    if (!event.locals.session) {
+  const session = event.locals.session;
+
+  if (pathRequiresAuth(event.url.pathname)) {
+    if (!session) {
       redirect(302, "/login");
     }
-    if (!event.locals.session.user.emailVerified) {
+    if (!session.user.emailVerified) {
       redirect(302, "/email-verification");
+    }
+  }
+
+  if (pathRequiresNoAuth(event.url.pathname)) {
+    if (session) {
+      // If user is not verified, redirect to email verification page
+      if (!session.user.emailVerified && event.url.pathname !== "/email-verification") {
+        redirect(302, "/email-verification");
+      }
+
+      // If user is already verified, redirect to home page
+      if (session.user.emailVerified) {
+        redirect(302, "/");
+      }
     }
   }
 
--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,1 +1,7 @@
+import type { LayoutServerLoad } from "./$types";
+
 export const ssr = false;
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+  return { user: locals.session?.user };
+}
