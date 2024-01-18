2a7a627fd44b1c23f9d7eaefcc111fe2e6cb272f
Mario Perez Hurtado <marioph10@gmail.com>
2024-01-18 18:04:01 +0100

    feat: delay button spinner

--- src/lib/ui/Button.svelte
+++ src/lib/ui/Button.svelte
@@ -6,16 +6,28 @@
   export let fullWidth = false;
   export let isLoading = false;
 
+  // Defer showing the spinner for 300ms to avoid flash
+  let showSpinner = false;
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  $: {
+    clearTimeout(timeout!);
+    timeout = setTimeout(() => (showSpinner = isLoading), 300);
+  }
+
   const SIZES = {
     small: "py-1.5 px-2.5 text-xs",
     medium: "py-1.5 px-3 text-sm",
   };
 
   const INTENTS = {
-    primary: "bg-cyan-600 border-cyan-600 hover:brightness-110 focus:outline-cyan-600 text-white",
+    primary:
+      "bg-cyan-600 border-cyan-600 hover:brightness-110 focus:outline-cyan-600 text-white",
     secondary: "bg-white focus:outline-cyan-200 text-zinc-600",
-    danger: "bg-red-500 hover:bg-red-600 border-red-500 focus:outline-red-600 text-white",
-    dangerSecondary: "bg-red-100 hover:bg-red-200 border-red-100 focus:outline-red-200 text-red-700",
+    danger:
+      "bg-red-500 hover:bg-red-600 border-red-500 focus:outline-red-600 text-white",
+    dangerSecondary:
+      "bg-red-100 hover:bg-red-200 border-red-100 focus:outline-red-200 text-red-700",
   };
 </script>
 
@@ -23,12 +35,12 @@
   {...$$restProps}
   class="w-fit border font-semibold rounded-md shadow-sm transition flex gap-2 items-center justify-center {$$restProps.class} {SIZES[size]} {INTENTS[intent]}"
   class:w-full={fullWidth}
-  class:opacity-50={$$restProps.disabled || isLoading}
+  class:opacity-50={$$restProps.disabled}
   class:pointer-events-none={$$restProps.disabled || isLoading}
   on:click
 >
   <slot />
-  {#if isLoading}
-    <Spinner class="w-3.5 h-3.5" />
+  {#if showSpinner}
+    <Spinner class="w-3.5 h-3.5 ml-0.5" />
   {/if}
 </button>
